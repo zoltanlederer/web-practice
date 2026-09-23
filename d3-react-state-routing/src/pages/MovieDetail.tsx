af3607365ab5035b4f007e5af40533f2c6613e43
Zoltan Lederer <23390588+zoltanlederer@users.noreply.github.com>
2026-09-23 22:39:12 +0200

    add movie list and detail pages with hardcoded data

--- d3-react-state-routing/src/pages/MovieDetail.tsx
+++ d3-react-state-routing/src/pages/MovieDetail.tsx
@@ -1,6 +1,27 @@
+import type { Movie } from '../types'
+import movies from '../data/movies.json'
+import { useParams } from "react-router-dom";
+import NotFound from './NotFound';
+
 function MovieDetail() {
+    const { id } = useParams()
+
+    const typedMovies = movies as Movie[];
+
+    const movie = typedMovies.find(movie => movie.id === id)
+
+    if(!movie) {
+        return (
+            <NotFound />
+        )
+    }
+
     return (
+        <>
         <h1>Movie detail</h1>
+        <p>Title: {movie.title}</p>
+        <p>Year: {movie.year}</p>
+        </>
     )
 }
 
--- d3-react-state-routing/src/pages/MovieList.tsx
+++ d3-react-state-routing/src/pages/MovieList.tsx
@@ -1,6 +1,19 @@
+import type { Movie } from '../types'
+import movies from '../data/movies.json'
+import { Link } from 'react-router-dom'
+
 function MovieList() {
+    const typedMovies = movies as Movie[];
+
     return (
+        <>
         <h1>Movie list</h1>
+        <ul>
+            {typedMovies.map(item => (
+                <li key={item.id}><Link to={`/movies/${item.id}`} >{item.title}</Link></li>
+            ))}
+        </ul>
+        </>
     )
 }
 
